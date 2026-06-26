7181ff80e36db5b50a357083ddf7af1e4f4ca221
lunatic-man <mishraishaan283@gmail.com>
2026-06-26 10:46:52 +0530

    feat: solve reverse-string exercise

---
+++ week2/reverse-string/reverse_string.py
@@ -0,0 +1,9 @@
+def reverse(text):
+    """This function is used to reverse the given text and output it as it is read from right to left.
+
+	Parameters:
+		text (str): The text that is to be reversed
+	Returns:
+		str: The reversed text in string format"""
+
+    return text[::-1]
