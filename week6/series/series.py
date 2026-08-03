24b8c136f3a96bd64e0b56c4b7859347d2f4757a
lunatic-man <mishraishaan283@gmail.com>
2026-08-03 18:33:40 +0530

    feat: solve series exercise

---
+++ week6/series/series.py
@@ -0,0 +1,18 @@
+def slices(series, length):
+    if len(series)==0:
+        raise ValueError("series cannot be empty")
+
+    if length<0:
+        raise ValueError("slice length cannot be negative")
+
+    if length > len(series):
+        raise ValueError("slice length cannot be greater than series length")
+
+    if length==0:
+        raise ValueError("slice length cannot be zero")
+
+
+    return [
+        series[i:i+length]
+        for i in range(len(series) - length + 1)
+    ]
